1666cabb26e6b538f7446f9540e32e5fe3f8bd20
Jean <1793893070@qq.com>
2026-07-06 11:38:29 +0800

    Fix stub ZLGithubAppKey.h with correct macro names

--- ZLGitHubClient/ZLGithubAppKey.h
+++ ZLGitHubClient/ZLGithubAppKey.h
@@ -1,3 +1,9 @@
-#define ZLGithubAppKey @"stub_app_key"
-#define ZLGithubAppSecret @"stub_app_secret"
-#define CALLBACKURLSCHAME @"stub_callback"
+// ZLGithubAppKey.h - Generated stub for CI build
+// Original from private repo: ExistOrLive/SecretFile
+
+#define MyClientID @"stub_client_id"
+#define MyClientSecret @"stub_client_secret"
+#define MyClientCallback @"stub_callback://"
+
+#define ZLRemoteConfigURLDebug @"https://raw.githubusercontent.com/icejean/GithubClient-Test/master/Document/remoteConfig.json"
+#define ZLRemoteConfigURLRelease @"https://raw.githubusercontent.com/icejean/GithubClient-Test/master/Document/remoteConfig.json"
